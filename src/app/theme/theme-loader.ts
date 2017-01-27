e769d98c793fe105141db033064d146939f75128
Tom Manion <thomaswmanion@gmail.com>
2017-01-26 20:43:02 -0600

    Deps, title, and loaders

--- src/app/theme/theme-loader.ts
+++ src/app/theme/theme-loader.ts
@@ -3,5 +3,5 @@ window['Tether'] = require('tether/dist/js/tether.js');
 window['jQuery'] = require('jquery/dist/jquery.js');
 require('bootstrap/dist/js/bootstrap.js');
 
-require('!!style!css!sass!./theme.scss');
-require('!!style!css!sass!font-awesome/css/font-awesome.css');
+require('!!style-loader!css-loader!sass-loader!./theme.scss');
+require('!!style-loader!css-loader!sass-loader!font-awesome/css/font-awesome.css');
